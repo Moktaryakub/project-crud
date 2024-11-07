bc75aefa837516eae8078bff60db232fcd605432
Moktar Yakub Osman <qanciyeofficial47@gmail.com>
2024-11-07 17:56:19 +0300

    update tasksConroller

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -20,19 +20,20 @@ exports.createTasks =(res,req)=>{
             }));
             return;
         }
+        const image = files.image[0];
         const tasks = readTasksFromFile()
         const newTask = {
             id: Data.now(),
             title: fields.title,
             description: fields.description,
             status: fields?.status || 'pending',
-            image: files.image?`/uploads/${files.image.name}`:null,
+            image: files.image?`/uploads/${files.newfilename}`:null,
         }
         tasks.push(newTask);
         writeTasksToFile(tasks);
 
         if (files.image) {
-            copyfilesync(files.image.path,path.joi(__dirname, '../uploads',files.image.name));
+            copyfilesync(image.filepath,path.joi(__dirname, '../uploads',files.image.name));
 
             res.end(JSON.stringify(newtasks));
         }
--- views/app.js
+++ views/app.js
@@ -1,17 +1,21 @@
-const http= require("http");
-const taskroot= require('./routes/taskroot');
-const HOSTNAME ="localhost";
-const port =6000
-const server = http.createServer( (req, res) => {
-    if (req.url.startswith(`/tasks`)) {
-        taskroot (req,url)
-    } else{
-        res.writeheader(404, "Not Found",{'Content-Type':   'application/json'       })
-        res.end(JSON.stringify({   
-            massage :'sorry,you got lost'
-        }))
+const http = require("http");
+const taskroot = require('./routes/taskroot');
+const HOSTNAME = "localhost";
+const port = 6000;
+
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith(`/tasks`)) {
+        // Wac taskroot oo sii req iyo res
+        taskroot(req, res);
+    } else {
+        // Qorka saxda ah ee writeHead
+        res.writeHead(404, "Not Found", {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            message: 'Sorry, you got lost'
+        }));
     }
 });
-server.listen(port,HOSTNAME,()=>{
+
+server.listen(port, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${port}/`);
 });
